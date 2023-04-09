217725a030e453f9a07d6e84dcd2bd018e37cbee
nicolas-limadev <nicolasbasto76@gmail.com>
2023-04-08 22:33:34 -0300

    init-studies-restful

---
+++ springstudies/src/main/java/com/nicklima/springstudies/SpringstudiesApplication.java
@@ -0,0 +1,13 @@
+package com.nicklima.springstudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstudiesApplication.class, args);
+	}
+
+}
---
+++ springstudies/src/test/java/com/nicklima/springstudies/SpringstudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicklima.springstudies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
